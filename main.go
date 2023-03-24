0e3290ef501b86394014a7b7ad5f55dae8caf318
yan.xue <15011272359@163.com>
2023-03-24 22:16:02 +0800

    change main.go

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"id-service/xid"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	guid := xid.New()
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": guid.String(),
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
