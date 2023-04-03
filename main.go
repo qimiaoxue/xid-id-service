da686536d1791f31f16af180cb14e180675cc622
yan.xue <15011272359@163.com>
2023-04-03 20:37:25 +0800

    rename google_uuid to uuid

--- main.go
+++ main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-	"id-service/xid"
-
 	"github.com/gin-gonic/gin"
+	"github.com/qimiaoxue/xid-id-service/uuid"
+	"github.com/qimiaoxue/xid-id-service/xid"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		guid := xid.New()
+		uid := uuid.New()
 		c.JSON(200, gin.H{
-			"message": guid.String(),
-			"Machine": guid.Machine(),
-			"Pid":     guid.Pid(),
-			"Time":    guid.Time(),
-			"Counter": guid.Counter(),
+			"xid_message":  guid.String(),
+			"Machine":      guid.Machine(),
+			"Pid":          guid.Pid(),
+			"Time":         guid.Time(),
+			"Counter":      guid.Counter(),
+			"uuid_message": uid,
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
